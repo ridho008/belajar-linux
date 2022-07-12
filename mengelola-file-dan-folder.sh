d21dd68af9df617296d979f1e1bb74859ab54b7c
ridho008 <ciuyy12@gmail.com>
2022-07-12 15:05:38 +0700

    mengelola file dan folder

---
+++ mengelola-file-dan-folder.sh
@@ -0,0 +1,36 @@
+
+
+# untuk melihat struktur hardisk
+lsblk
+
+# change directory
+cd /home/belajarlinux
+
+# mengetahui lokasi sekarang (berada di folder mana) -> Print Working Directory
+pwd
+
+# melihat daftar isi folder
+ls 
+
+# melihat daftar isi folder dan hak aksesnya
+ls -l
+
+# membuat file kosong
+touch nama_file
+touch namafile1 namafile2 namafile3
+
+# membuat folder kosong
+mkdir
+
+# copy paster
+cp /media/asus/06D0D488D0D47EFD/Belajar-Linux/coba /media/asus/06D0D488D0D47EFD/Belajar-Linux/coba1
+cp /path/file/asal /path/file/tujuan
+
+# untuk memindahkan dan merename file atau folder
+mv /path/to/asal /path/to/tujuan
+
+# menghapus file/folder beserta isi
+rm -f folder/file
+
+# melihat use storage file/folder
+du 
